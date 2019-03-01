a6628b699aa310179d570187479a2989c5271f65
Yury Laykov <sowulo@inbox.ru>
2019-03-01 13:30:50 +0300

    Added no type operation

---
+++ Operations/No type operation.js
@@ -0,0 +1,52 @@
+_operation = {
+	"version": "1.0",
+	"name": "NoTypeOperation",
+	"title": {
+		"en": "No Type Operation",
+		"ru": "Операция без типа"
+	},
+	"category": {
+		"name": "General",
+		"title": {
+			"en": "General",
+			"ru": "Общее"
+		}
+	},
+	"subcategory": {
+		"name": "Arbitrary",
+		"title": {
+			"en": "Arbitrary Operations",
+			"ru": "Произвольные операции"
+		}
+	},
+	"fields": [
+		{
+			"name": "Title",
+			"title": {
+				"en": "Title",
+				"ru": "Название"
+			},
+			"type": "text"
+		},
+		{
+			"name": "Description",
+			"title": {
+				"en": "Description",
+				"ru": "Описание"
+			},
+			"type": "textarea"
+		},
+		{
+			"name": "Files",
+			"title": {
+				"en": "Files",
+				"ru": "Файлы"
+			},
+			"type": "file",
+			"attributes": [{
+				"name": "multiple",
+				"value": ""
+			}]
+		}
+	]
+};
--- list.js
+++ list.js
@@ -1,3 +1,4 @@
 var _OperationsPathList = [
+	"Operations/No type operation.js",
 	"Operations/Party admission application.js"
 ];
